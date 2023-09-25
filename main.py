d43ce0bc045badc1923338846983af27b7515ac4
KingPungy <scott.timmermans@gmail.com>
2023-09-25 21:45:21 +0200

    updated TODO in main.py

--- main.py
+++ main.py
@@ -72,10 +72,11 @@ def main() -> None:
                    humidity=20.0, room_dimensions=[10, 10, 2])
     # make higher order function to pass in the room object
 
-    TODO:
+    #TODO:
         # Instantiate MockArduino class with connected sensors and actuators
         # instantiate the room object and pass it to the MockArduino class
-            # MockArduino(com_port='COM3', Room_sim_class_variables, temperature_sensor_pins=['d:7:i','d:6:i'], light_sensor_pin='A:0:i', heater_pin='d:31:o', cooler_pin='d:31:o', Sunscreen_pin='d:31:o')
+            # MockArduino(com_port='COM3', Room_simulator_object)
+            # setpin modes for the sensors and actuators
             
         # pass the mockArduino class to the mockFrirmata class to simulate a connection to the arduino
 
